d6a60c5f1ceeb699daf2714c2b95e7e9662f8a0c
praveen-tech-workspace <praveen.tech.workspace@gmail.com>
2025-12-17 12:15:05 +0530

    Delete payments directory

--- payments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-    path("payment/", views.payment_page, name="payment"),
-    path("sucess/", views.paysucess, name="paysucess"),
-    
-]
